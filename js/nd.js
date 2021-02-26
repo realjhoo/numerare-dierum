77b405ce0912552f44816dafe3a27c3ef1a641f8
Jerry L Hoover <jerry.hoover@ymail.com>
2021-02-25 21:36:46 -0600

    71 Duomelia Update

--- js/nd.js
+++ js/nd.js
@@ -1,5 +1,19 @@
+const meliaName = [
+  "Nullamelia",
+  "Unumelia",
+  "Duomelia",
+  "Triamelia",
+  "Quattarmelia",
+  "Quinquemelia",
+  "Sexmelia",
+  "Septmelia",
+  "Octomelia",
+  "Novemelia",
+];
+
 // --------------------------------------------------------
 function getJulianDay() {
+  // gets the julian date, off set UTC+1 tho
   let today = new Date();
   let year = today.getUTCFullYear(),
     month = today.getUTCMonth(),
@@ -15,7 +29,7 @@ function getJulianDay() {
     date++;
   }
 
-  // calculate julian date
+  // calculate julian date - thanks stackoverflow
   let a = (14 - month) / 12;
   let y = year + 4800 - a;
   let m = month + 12 * a - 3;
@@ -23,12 +37,11 @@ function getJulianDay() {
   let JD =
     date + (153 * m + 2) / 5 + y * 365 + y / 4 - y / 100 + y / 400 - 32045;
 
-  // remove the fractional bit. We'll replace with beats.
+  // remove the fractional bit. We'll replace with beats later
   let remainder = JD % 1;
   JD -= remainder;
 
   return JD;
-  // for 2021.02.25 -> expected output = 2459270
 }
 
 // --------------------------------------------------------
@@ -40,8 +53,7 @@ function getBeats() {
     minutes = time.getUTCMinutes(),
     hours = time.getUTCHours();
 
-  // correct for "Biel Mean Time" i.e.,
-  // Central European Standard -> UTC + 1
+  // correct for Central European Standard -> UTC + 1
   if (hours === 23) {
     hours = 0;
   } else {
@@ -50,7 +62,7 @@ function getBeats() {
 
   let secondsSoFar = convertToSeconds(hours, minutes, seconds);
 
-  // * * * convert to swatch internet time * * *
+  // * * * convert to beats, aka internet time * * *
   // the next 4 lines of code could be combined. Separated for clarity
   // chop excessive precision
   let internetTime = (secondsSoFar / secondsPerBeat).toFixed(2);
@@ -61,8 +73,6 @@ function getBeats() {
   // add the @
   internetTime = "@" + internetTime;
 
-  // log out displays on the menu bar
-  //  console.log(internetTime);
   return internetTime;
 }
 
@@ -72,19 +82,63 @@ function convertToSeconds(hours, minutes, seconds) {
 }
 
 // --------------------------------------------------------
-let ND = getJulianDay(25, 2, 2021);
-let thebeats = getBeats();
+function sliceJD(ND) {
+  // extract elements from the JD
+  let triennium = ND.toString().slice(0, 4);
+  let melia = ND.toString().slice(4, 5);
+  let centum = ND.toString().slice(5, 7);
 
-console.log(ND + thebeats);
+  return [triennium, melia, centum];
+}
 
+// --------------------------------------------------------
+function showNumerareDierum(triennium, melia, centum, thebeats) {
+  let today = new Date();
+  let newDate = today.toISOString().slice(0, 10);
+
+  const output = document.querySelector(".output");
+  // <span class="red">${newDate}</span>,
+  output.innerHTML = `Today, is the ${centum}th day of ${meliaName[melia]} in Triennium ${triennium}.</p> <p>More concisely, ${triennium}-${melia}-${centum}.</p> <p>The time, in beats, may be appended, thusly ${triennium}-${melia}-${centum}${thebeats}</p> `;
+}
+
+// --------------------------------------------------------
+function toggleit() {
+  let toggleButton = document.getElementById("toggle-button");
+  let explainer = document.querySelector(".explainer");
+
+  if (toggleButton.innerHTML === "Show More") {
+    toggleButton.innerHTML = "Show Less";
+    explainer.style.display = "block";
+  } else {
+    toggleButton.innerHTML = "Show More";
+    explainer.style.display = "none";
+  }
+}
+
+// --------------------------------------------------------
+function main() {
+  let ND = getJulianDay();
+  let thebeats = getBeats();
+  let [triennium, melia, centum] = sliceJD(ND);
+
+  showNumerareDierum(triennium, melia, centum, thebeats);
+
+  console.log(triennium, meliaName[melia], centum);
+  console.log(triennium, meliaName[melia], centum + " " + thebeats);
+  console.log(ND);
+}
+
+// --------------------------------------------------------
+main();
 /* 
 TODO: 
-Place lose code in functions... I hate lose code!!!
-Output the results to the web page
-Make page pretty for gods' sake
-Rewrite blurb so it doesnt want to show current Numerare Dierum multiple times
+x Place lose code in functions... I hate lose code!!!
+How to do ordinal identifiers?
+x Output the results to the web page
+Make page pretty for gods' sake sorta...
+x Rewrite blurb so it doesnt want to show current Numerare Dierum multiple times
 Run the current and updating time at the top
-Adapt the code to the bitbar beats :-)
+x Adapt the code to the bitbar beats :-)
 Deploy this page to saxondate.com using the menu bar there
 Rewrite the menu bar there. Dev Ed?
 */
